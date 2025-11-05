4332b89b57e8876ce7a2c66f1463d4bbe61ccfae
AndreReyesG <jandre.reyesg@gmail.com>
2025-11-04 21:58:20 -0600

    feat(blogrenderer): render the post's title as <h1>

---
+++ internal/blogrenderer/blogrenderer.go
@@ -0,0 +1,13 @@
+package blogrenderer
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/AndreReyesG/blogrenderer/internal/blogposts"
+)
+
+func Render(w io.Writer, p blogposts.Post) error {
+	_, err := fmt.Fprintf(w, "<h1>%s</h1>", p.Title)
+	return err
+}
